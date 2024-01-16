84094d5107176731795956ce0106e6d04a5cd8a8
Abdalmohsen-H <92123245+Abdalmohsen-H@users.noreply.github.com>
2024-01-17 01:50:08 +0200

    fix type in task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import asyncio
 import random
-from typing import Generator
+from typing import async_generator
 
 
-async def async_generator() -> Generator[float, None]:
+async def async_generator() -> async_generator[float, None, None]:
     """ func documentation here just random gen. """
     for _ in range(10):
         # await with asyncio for 1 sec asynchronously
