733a21fabb01b7367331474b1a549fcd94ed61b8
Abdalmohsen-H <92123245+Abdalmohsen-H@users.noreply.github.com>
2024-01-17 01:52:09 +0200

    task0 update

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 """ task 0 : async generator Module doc. """
-
 import asyncio
 import random
 from typing import async_generator
 
 
 async def async_generator() -> async_generator[float, None, None]:
-    """ func documentation here just random gen. """
+    """ func documentation here just random gen. 
+    The return type of generator functions can be
+    annotated by the generic type Generator[yield_type,
+    send_type, return_type] provided by typing.py module
+    """
     for _ in range(10):
         # await with asyncio for 1 sec asynchronously
         await asyncio.sleep(1)
