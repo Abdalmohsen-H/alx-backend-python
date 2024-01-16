e1188cf4ef181bfed89ae6aab726366f35d75ed4
Abdalmohsen-H <92123245+Abdalmohsen-H@users.noreply.github.com>
2024-01-17 01:52:48 +0200

    fix task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -12,6 +12,5 @@ async def async_generator() -> async_generator[float, None, None]:
     send_type, return_type] provided by typing.py module
     """
     for _ in range(10):
-        # await with asyncio for 1 sec asynchronously
         await asyncio.sleep(1)
         yield random.uniform(0, 10)    
