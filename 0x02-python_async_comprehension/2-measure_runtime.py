0f49d02abe0f585d398ce5726742a51b47450874
Abdalmohsen-H <92123245+Abdalmohsen-H@users.noreply.github.com>
2024-01-17 00:17:02 +0000

    task 2 : measure runtime

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" task 2 Module : measure runtime of async comprehension
+ generator """
+import asyncio
+import time
+
+async_comp_gen = __import__("1-async_comprehension").async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Function to measure runtime of async comprehension
+    generator """
+    start_timestamp = time.perf_counter()
+    await asyncio.gather(async_comp_gen(), async_comp_gen(), async_comp_gen(),
+                            async_comp_gen()
+                         )
+    # or
+    # jobs = [async_comp_gen() for _ in range(4)]
+    # await asyncio.gather(*jobs)
+
+    end_timestamp = time.perf_counter()
+    duration = end_timestamp - start_timestamp
+    return duration
